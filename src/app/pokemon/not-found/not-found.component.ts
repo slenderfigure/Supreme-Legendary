882b77de25884c05c9906e612796010ff6195a4f
Adison Pena <hirayoki.nova@gmail.com>
2020-05-13 18:06:59 -0400

    Remove unnecessary clutter

--- src/app/pokemon/not-found/not-found.component.ts
+++ src/app/pokemon/not-found/not-found.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ViewChild, ElementRef } from '@angular/core';
 
-import { Observable, of, from, fromEvent, Subscription } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map, filter, tap, debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
-
 
 @Component({
   selector: 'app-not-found',
@@ -13,34 +9,17 @@ import { map, filter, tap, debounceTime, distinctUntilChanged, switchMap } from 
 })
 export class NotFoundComponent implements OnInit, AfterViewInit {
   @ViewChild('box') box: ElementRef<HTMLElement>;
-  @ViewChild('inputField') inputField: ElementRef<HTMLInputElement>;
   @ViewChild('square') square: ElementRef<HTMLElement>;
   @ViewChild('circle') circle: ElementRef<HTMLElement>;
-
-
-  abilities: string[] = [];
   
-  getAbilities(): Observable<string[]> {
-    return this.http.get<string[]>('../../api/abilities.json');
-  }
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    fromEvent(this.inputField.nativeElement, 'input').pipe(
-      debounceTime(2000),
-      distinctUntilChanged(),
-      switchMap(() => this.getAbilities())
-    ).subscribe((abilities: string[]) => {
-      const value = this.inputField.nativeElement.value.trim().toLowerCase();
-
-      this.abilities = abilities.filter(val => val.slice(0, value.length).toLowerCase() == value);
-      
-      console.log(this.abilities);
-    });
+    
   }
 
   activeRipple(e: MouseEvent): void {
