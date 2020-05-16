19edb52a65f8f161374963dd89a645457fabf98e
Adison Pena <hirayoki.nova@gmail.com>
2020-05-16 18:24:40 -0400

    Fixed evolution ASC sorting

--- src/app/pokemon/pokemon-details/evolutions/evolutions.component.ts
+++ src/app/pokemon/pokemon-details/evolutions/evolutions.component.ts
@@ -59,7 +59,7 @@ export class EvolutionsComponent implements OnInit, OnDestroy, OnChanges {
       this.ps.getPokedex().subscribe((pokedex: Pokemon[]) => {
         this.evolutions = pokedex.filter(pokemon => {
           return this.pokemon[this.lineType].indexOf(+pokemon.entryNumber) > -1;
-        }).sort((a, b) => a.evolutionOrder - b.evolutionOrder);
+        }).sort((a, b) => +a.entryNumber - +b.entryNumber);
       });
     }
   }
